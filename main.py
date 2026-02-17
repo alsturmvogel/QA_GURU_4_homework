7a6b76404cb0a3a997ddd9ed3db844f2cac797ed
Alena Gushcha <g.aliena@mail.ru>
2026-02-17 22:51:50 +0300

    add .idea in .gitignore and create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+
+
+email = {
+    "subject": "Quarterly Report",
+    "from": "Alice.Cooper@Company. ",
+    "to": " bob_smith@Gmail.com ",
+    "body": "Hello Bob,\n\tHere is the quarterly report.\n\tPlease review and let me know your feedback.\n\nBest,\nAlice"
+}
